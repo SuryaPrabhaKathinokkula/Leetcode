b010a322f85600b8d65a70992e7ba853f5777579
K B SURYA PRABHA <23MH1A0524@acoe.edu.in>
2025-09-23 18:48:28 +0530

    Create 2942-FindWordsContainingCharacter

---
+++ 2942-FindWordsContainingCharacter/2942-FindWordsContainingCharacter.java
@@ -0,0 +1,14 @@
+// Last updated: 9/23/2025, 6:48:28 PM
+class Solution 
+{
+    public List<Integer> findWordsContaining(String[] words, char x)
+     {
+        List<Integer> res=new ArrayList<>();
+        for(int i=0;i<words.length;i++)
+        {
+         if(words[i].contains(""+x))
+         res.add(i);
+        }
+        return res;
+     }
+}
