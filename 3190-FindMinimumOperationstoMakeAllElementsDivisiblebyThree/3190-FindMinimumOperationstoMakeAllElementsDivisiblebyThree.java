2fff977792f76a7b4fc08886349c06566be0472e
K B SURYA PRABHA <23MH1A0524@acoe.edu.in>
2026-02-19 16:30:38 +0530

    File updated at 2/19/2026, 4:30:38 PM

--- 3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree/3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
+++ 3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree/3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
@@ -1,14 +1,14 @@
-// Last updated: 2/19/2026, 4:22:01 PM
+// Last updated: 2/19/2026, 4:30:38 PM
 1class Solution {
-2    public int minimumOperations(int[] nums) {
-3     int operations=0;
+2    public int numIdenticalPairs(int[] nums) {
+3     int pairs=0;
 4     for(int i=0;i<nums.length;i++){
-5        int remainder= nums[i]%3;
-6        if(remainder !=0){
-7            operations+=1;
-8        }
-9     }
-10     return operations;
-11     
+5        for(int j=i+1;j<nums.length;j++){
+6            if(nums[i]==nums[j]){
+7                pairs++;
+8            }
+9        }
+10     } 
+11     return pairs;  
 12    }
 13}
